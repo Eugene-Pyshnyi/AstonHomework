580044c3a41b922e437fda522056ff430082532f
Eugene Pyshnyi <pinch15975@gmail.com>
2024-06-27 17:57:28 +0500

    Lesson 9 is done

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private final Map<String, ArrayList<String>> phoneMap;
+
+    public PhoneBook() {
+        this.phoneMap = new HashMap<>();
+    }
+
+    public void add(String lastName, String phoneNumber) {
+        if (phoneMap.containsKey(lastName)) {
+            phoneMap.get(lastName).add(phoneNumber);
+        } else {
+            ArrayList<String> phoneNumbers = new ArrayList<>();
+            phoneNumbers.add(phoneNumber);
+            phoneMap.put(lastName, phoneNumbers);
+        }
+    }
+
+    public ArrayList<String> get(String lastName) {
+        return phoneMap.getOrDefault(lastName, new ArrayList<>());
+    }
 }
--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -3,11 +3,23 @@ import java.util.Arrays;
 
 public class Runner {
     public static void main(String[] args) {
+        //task 1
         UniqueWordCounter counter = new UniqueWordCounter();
         ArrayList<String> words = new ArrayList<>(Arrays.asList("hello", "world", "hello", "cat", "dog", "capacity",
                 "downloading", "cat", "turtle", "eagle", "swear",
                 "sewer", "rat", "capacity", "war", "limit", "hello", "dog", "war"));
 
         counter.count(words);
+        // разделение вывода задач в консоль
+        System.out.println();
+        // task 2
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Semenov", "+78005553535");
+        phoneBook.add("Semenov", "+79990030405");
+        phoneBook.add("Petrov", "+79992874546");
+        phoneBook.add("Alekseyev", "+79987874541");
+        System.out.println(phoneBook.get("Semenov"));
+        System.out.println(phoneBook.get("Petrov"));
+        System.out.println(phoneBook.get("Alekseyev"));
     }
 }
