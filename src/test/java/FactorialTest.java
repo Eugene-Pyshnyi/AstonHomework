dfe9f75c53632f30ed306af6f3473b6f7966809b
Eugene Pyshnyi <pinch15975@gmail.com>
2024-07-05 15:56:25 +0500

    Removed extra imports

--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -1,8 +1,8 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.testng.AssertJUnit.assertEquals;
 
 public class FactorialTest {
     private Factorial factorial;
