66464f7edbfda3f42435f6461f5ee27b38a0a9a0
Eugene Pyshnyi <pinch15975@gmail.com>
2024-07-28 04:17:02 +0500

    GetRequestTest is done

--- src/test/java/lesson16/api/GetRequestTest.java
+++ src/test/java/lesson16/api/GetRequestTest.java
@@ -10,6 +10,7 @@ public class GetRequestTest {
     @Test
     void getRequest() {
         Specifications.installSpecs(Specifications.requestSpec(), Specifications.responseSpec200());
+
         given()
                 .when()
                 .get("get?foo1=bar1&foo2=bar2")
