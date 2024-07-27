b8184e2877f6bcd1fb62612daca646e3492be4b4
Eugene Pyshnyi <pinch15975@gmail.com>
2024-07-27 23:22:08 +0500

    GetRequestTest is done

--- src/test/java/lesson16/api/GetRequestTest.java
+++ src/test/java/lesson16/api/GetRequestTest.java
@@ -3,9 +3,8 @@ package lesson16.api;
 import lesson16.Specifications;
 import org.testng.annotations.Test;
 
-
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 
 public class GetRequestTest {
     @Test
@@ -17,6 +16,14 @@ public class GetRequestTest {
                 .then().log().body() //На всякий случай вывожу в консоль тело ответа
                 .assertThat()
                 .body("args.foo1", is("bar1"))
-                .and().body("args.foo2", is("bar2"));
+                .and().body("args.foo2", is("bar2"))
+                .and().body("headers.host", is("postman-echo.com"))
+                .and().body("headers.x-request-start", instanceOf(String.class))
+                .and().body("headers.x-forwarded-proto", is("https"))
+                .and().body("headers.x-amzn-trace-id", instanceOf(String.class))
+                .and().body("headers.user-agent", instanceOf(String.class))
+                .and().body("headers.accept", is("*/*"))
+                .and().body("headers.accept-encoding", instanceOf(String.class))
+                .and().body("url", is("https://postman-echo.com/get?foo1=bar1&foo2=bar2"));
     }
 }
